a434492647fb1cb31e662821c8cb5bf318fccf82
liviu274 <liviululi@gmail.com>
2024-05-20 20:00:55 +0300

    Add project files.

---
+++ Console Output Handler/MainPage.cpp
@@ -0,0 +1,65 @@
+﻿#include "pch.h"
+#include "MainPage.h"
+#include "MainPage.g.cpp"
+
+
+using namespace winrt;
+using namespace Windows::Foundation;
+using namespace Windows::Storage;
+using namespace Windows::Storage::Pickers;
+using namespace Windows::UI::Xaml;
+
+
+
+namespace winrt::Console_Output_Handler::implementation
+{
+    int32_t MainPage::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainPage::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainPage::pickFileButton_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Windows::UI::Xaml::RoutedEventArgs const& e)
+    {
+        pickFileButton().Content(box_value(L"clicked"));
+        PickExecutableFile();
+
+        for (const auto &filePath : myFilePaths)
+            OutputDebugString((filePath + L"\n").c_str());
+    }
+
+    void MainPage::PickExecutableFile()
+    {
+        // Initialize the FileOpenPicker
+
+        FileOpenPicker picker;
+        picker.ViewMode(PickerViewMode::List);
+        picker.SuggestedStartLocation(PickerLocationId::Desktop);
+        picker.FileTypeFilter().Append(L".exe");
+
+        // Show the picker and get the selected file
+        picker.PickSingleFileAsync().Completed([this](IAsyncOperation<StorageFile> const& operation, AsyncStatus const status) {
+            if (status == AsyncStatus::Completed)
+            {
+                StorageFile file = operation.GetResults();
+                if (file)
+                {
+                    // Handle the selected file
+                    myFilePaths.push_back(file.Path());
+                    // Do something with the file path (e.g., display it, use it in your app, etc.)
+                    OutputDebugString(file.Path().c_str());
+                }
+            }
+            });
+    }
+}
+
+
+
+
+
+
---
+++ Console Output Handler/MainPage.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+
+#include "MainPage.g.h"
+
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Storage.h>
+#include <winrt/Windows.Storage.Pickers.h>
+#include <winrt/Windows.Storage.Streams.h>
+#include <winrt/Windows.UI.Xaml.h>
+#include <winrt/Windows.UI.Xaml.Controls.h>
+
+
+
+
+
+namespace winrt::Console_Output_Handler::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage()
+        {
+            // Xaml objects should not call InitializeComponent during construction.
+            // See https://github.com/microsoft/cppwinrt/tree/master/nuget#initializecomponent
+        }
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void ClickHandler(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+
+        void pickFileButton_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Windows::UI::Xaml::RoutedEventArgs const& e);
+        void PickExecutableFile();
+
+    private:
+        std::vector <winrt::hstring> myFilePaths;
+    };
+}
+
+namespace winrt::Console_Output_Handler::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
+
